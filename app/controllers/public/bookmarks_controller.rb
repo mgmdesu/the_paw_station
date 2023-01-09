050755028a6fb92ed674b1f2f47c146c92df4773
mgmdesu <115972724+mgmdesu@users.noreply.github.com>
2023-01-09 14:14:42 +0000

    [update]public_walkブックマーク機能追加

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -1,2 +1,16 @@
 class Public::BookmarksController < ApplicationController
+  
+  def create
+    walk = Walk.find(params[:walk_id])
+    bookmark = current_user.bookmarks.new(walk_id: walk.id)
+    bookmark.save
+    redirect_to walk_path(walk.id)
+  end 
+  
+  def destroy
+    walk = Walk.find(params[:walk_id])
+    bookmark = current_user.bookmarks.find_by(walk_id: walk.id)
+    bookmark.destroy
+    redirect_to walk_path(walk.id)
+  end 
 end
