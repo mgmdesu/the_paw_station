633e783603b6cd14b913e845da8458dc01fd1c75
mgmdesu <115972724+mgmdesu@users.noreply.github.com>
2023-01-15 09:23:59 +0000

    [update]ログイン後遷移先

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -52,8 +52,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    @user = User.find(params[:id])
-    user_path(@user.id)
+    root_path
   end
 
   # The path used after sign up for inactive accounts.
