a3603d090173b1bd8679de0124a2740a352c1cd2
mgmdesu <115972724+mgmdesu@users.noreply.github.com>
2023-01-18 05:32:13 +0000

    [Add]いいね/ブックマーク非同期通信化

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,16 +1,14 @@
 class Public::FavoritesController < ApplicationController
   
   def create
-    post_dog = PostDog.find(params[:post_dog_id])
-    favorite = current_user.favorites.new(post_dog_id: post_dog.id)
+    @post_dog = PostDog.find(params[:post_dog_id])
+    favorite = current_user.favorites.new(post_dog_id: @post_dog.id)
     favorite.save
-    redirect_to post_dog_path(post_dog.id)
   end
   
   def destroy
-    post_dog = PostDog.find(params[:post_dog_id])
-    favorite = current_user.favorites.find_by(post_dog_id: post_dog.id)
+    @post_dog = PostDog.find(params[:post_dog_id])
+    favorite = current_user.favorites.find_by(post_dog_id: @post_dog.id)
     favorite.destroy
-    redirect_to post_dog_path(post_dog.id)
   end 
 end
