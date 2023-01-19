15114cf0f64113c1e5b200c7c4cc7e4987adf1c0
mgmdesu <115972724+mgmdesu@users.noreply.github.com>
2023-01-19 05:11:53 +0000

    [Add]フォローフォロワー機能/カウント_非同期通信化

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -3,27 +3,25 @@ class Public::RelationshipsController < ApplicationController
   
   #フォローする時
   def create
-    user = User.find(params[:user_id])
-    current_user.follow(user)
-    redirect_to request.referer
+    @user = User.find(params[:user_id])
+    current_user.follow(@user)
   end 
   
   #フォロー外す時
   def destroy
-    user = User.find(params[:user_id])
-    current_user.unfollow(user)
-    redirect_to request.referer
+    @user = User.find(params[:user_id])
+    current_user.unfollow(@user)
   end 
   
   #フォロー一覧
   def followings
-    user = User.find(params[:user_id])
-    @users = user.followings
+    @user = User.find(params[:user_id])
+    @users = @user.followings
   end 
   
   #フォロワー一覧
   def followers
-    user = User.find(params[:user_id])
-    @users = user.followers
+    @user = User.find(params[:user_id])
+    @users = @user.followers
   end 
 end
