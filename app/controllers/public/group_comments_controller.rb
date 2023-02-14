765ad015be7ba1534e876e20baf71e02bdef99b2
mgmdesu <115972724+mgmdesu@users.noreply.github.com>
2023-02-14 06:56:30 +0000

    [Add]public/group_comment/コメント非同期通信化

--- app/controllers/public/group_comments_controller.rb
+++ app/controllers/public/group_comments_controller.rb
@@ -1,16 +1,17 @@
 class Public::GroupCommentsController < ApplicationController
   
   def create
-    group = Group.find(params[:group_id])
-    comment = current_user.group_comments.new(group_comment_params)
-    comment.group_id = group.id
-    comment.save
-    redirect_to group_path(group)
+    @group = Group.find(params[:group_id])
+    @group_comment = GroupComment.new(group_comment_params)
+    @group_comment.group_id = @group.id
+    @group_comment.user_id = current_user.id
+    @group_comment.save
   end 
   
   def destroy
-    GroupComment.find(params[:id]).destroy
-    redirect_to group_path(params[:group_id])
+    @group = Group.find(params[:group_id])
+    @group_comment = GroupComment.find(params[:id])
+    @group_comment.destroy
   end
   
   private
