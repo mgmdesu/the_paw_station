b058b5a638323a96e5e43acb2ef5be5c58264ee0
mgmdesu <115972724+mgmdesu@users.noreply.github.com>
2023-02-14 06:10:03 +0000

    [Add]admin/post_dog/コメント非同期通信化

--- app/controllers/admin/post_comments_controller.rb
+++ app/controllers/admin/post_comments_controller.rb
@@ -2,7 +2,10 @@ class Admin::PostCommentsController < ApplicationController
   before_action :authenticate_admin!
   
   def destroy
-    PostComment.find(params[:id]).destroy
-    redirect_to admin_post_dog_path(params[:post_dog_id])
+    @post_dog = PostDog.find(params[:post_dog_id])
+    @post_comment = PostComment.find(params[:id])
+    @post_comment.destroy
+    # PostComment.find(params[:id]).destroy
+    # redirect_to admin_post_dog_path(params[:post_dog_id])
   end
 end
