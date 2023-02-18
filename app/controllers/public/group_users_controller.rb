8d5911793b7848e4e10176c3fe847423719035f7
mgmdesu <115972724+mgmdesu@users.noreply.github.com>
2023-02-18 06:22:23 +0000

    [Add]参加退会ボタン非同期通信化

--- app/controllers/public/group_users_controller.rb
+++ app/controllers/public/group_users_controller.rb
@@ -4,12 +4,12 @@ class Public::GroupUsersController < ApplicationController
   def create
     group_user = current_user.group_users.new(group_id: params[:group_id])
     group_user.save
-    redirect_to request.referer
+    @group = Group.find(params[:group_id])
   end
   
   def destroy
     group_user = current_user.group_users.find_by(group_id: params[:group_id])
     group_user.destroy
-    redirect_to request.referer
+    @group = Group.find(params[:group_id])
   end
 end
